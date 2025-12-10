9cab8d2e983e5de788f43a8b18cda454459ba91e
spebelgenenst <sebastian@die-sperlinge.de>
2025-12-10 16:45:48 +0100

    modified:   test.py

--- test.py
+++ test.py
@@ -8,10 +8,11 @@ with open('credentials.json', 'r') as file:
 github_token = Auth.Token(credentials["GithubToken"])
 g = Github(auth=github_token)
 
-repo = g.get_repo("Spebelgenenst/ai-experiment")
+repo = g.get_repo("kaibuzz0/Termux-Mobile-BAREIRON-server")
 
 open_issues = repo.get_issues(state='open')
 
-print(open_issues)
+for issue in open_issues:
+    print(issue)
 
 g.close
